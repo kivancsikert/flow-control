0f45b5cbf9d446eaf7440e203913427dd34515fd
Lóránt Pintér <lorant.pinter@gmail.com>
2021-10-27 15:15:05 +0200

    Sleep if there's no flow

--- src/main.cpp
+++ src/main.cpp
@@ -3,10 +3,12 @@
 #include <FlowMeter.h>
 #include <Wire.h>
 
-const int FLOW_PIN = GPIO_NUM_33;
+const gpio_num_t FLOW_PIN = GPIO_NUM_33;
 
 // get a new FlowMeter instance for an uncalibrated flow sensor
 FlowMeter* meter;
+
+long long lastSeenFlow = 0;
 
 IRAM_ATTR void meterCount() {
     meter->count();
@@ -22,12 +24,27 @@ void setup() {
 
 void loop() {
     // process the (possibly) counted ticks
-    delay(200);
-    meter->tick(200);
+    delay(1000);
+    meter->tick(1000);
+
+    double flowRate = meter->getCurrentFlowrate();
+    auto currentTime = millis();
+    if (flowRate > 0.0) {
+        Serial.print("Flow rate: ");
+        Serial.println(flowRate);
+        lastSeenFlow = currentTime;
+    } else {
+        if (currentTime - lastSeenFlow > 5000) {
+            Serial.println("No flow for 5 seconds, going to sleep");
+            // Go to deep sleep until woken up by GPIO interrupt
+            esp_sleep_enable_ext0_wakeup(FLOW_PIN, digitalRead(FLOW_PIN) == LOW);
+            esp_deep_sleep_start();
+        }
+    }
 
     // output some measurement result
     Serial.print("Currently ");
-    Serial.print(meter->getCurrentFlowrate());
+    Serial.print(flowRate);
     Serial.print(" l/min, ");
     Serial.print(meter->getTotalVolume());
     Serial.println(" l total.");
