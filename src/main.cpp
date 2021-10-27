4cf5ef1475050a927353c201399cde42cbe66f16
Lóránt Pintér <lorant.pinter@gmail.com>
2021-10-27 13:13:37 +0200

    Measure flow

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
+// include the library code:
 #include <Arduino.h>
+#include <FlowMeter.h>
+#include <Wire.h>
+
+const int FLOW_PIN = GPIO_NUM_19;
+
+// get a new FlowMeter instance for an uncalibrated flow sensor
+FlowMeter* meter;
+
+IRAM_ATTR void meterCount() {
+    meter->count();
+}
 
 void setup() {
-  // put your setup code here, to run once:
+    Serial.begin(115200);
+    Serial.println();
+    Serial.println("Hello");
+
+    meter = new FlowMeter(digitalPinToInterrupt(FLOW_PIN), UncalibratedSensor, meterCount, RISING);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    // process the (possibly) counted ticks
+    delay(200);
+    meter->tick(200);
+
+    // output some measurement result
+    Serial.print("Currently ");
+    Serial.print(meter->getCurrentFlowrate());
+    Serial.print(" l/min, ");
+    Serial.print(meter->getTotalVolume());
+    Serial.println(" l total.");
 }
