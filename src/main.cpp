d9366cd774e7097cd94a533d482f2cbaf31aea79
Lóránt Pintér <lorant.pinter@gmail.com>
2021-10-27 15:47:58 +0200

    Tidy up WiFiManager init

--- src/main.cpp
+++ src/main.cpp
@@ -25,37 +25,24 @@ IRAM_ATTR void meterCount() {
 void setup() {
     Serial.begin(115200);
     Serial.println();
-    Serial.println("Hello");
 
-    WiFi.mode(WIFI_STA);    // explicitly set mode, esp defaults to STA+AP
-    // it is a good practice to make sure your code sets wifi mode how you want it.
+    // Explicitly set mode, ESP defaults to STA+AP
+    WiFi.mode(WIFI_STA);
 
-    // put your setup code here, to run once:
-    Serial.begin(115200);
-
-    //WiFiManager, Local intialization. Once its business is done, there is no need to keep it around
     WiFiManager wm;
 
-    // reset settings - wipe stored credentials for testing
-    // these are stored by the esp library
+    // Reset settings - wipe stored credentials for testing;
+    // these are stored by the ESP library
     //wm.resetSettings();
 
     // Automatically connect using saved credentials,
-    // if connection fails, it starts an access point with the specified name ( "AutoConnectAP"),
-    // if empty will auto generate SSID, if password is blank it will be anonymous AP (wm.autoConnect())
-    // then goes into a blocking loop awaiting configuration and will return success result
-
-    bool res;
-    // res = wm.autoConnect(); // auto generated AP name from chipid
-    // res = wm.autoConnect("AutoConnectAP"); // anonymous ap
-    res = wm.autoConnect("AutoConnectAP", "password");    // password protected ap
-
-    if (!res) {
+    // if connection fails, it starts an access point
+    // with an auto-generated SSID and no password,
+    // then goes into a blocking loop awaiting
+    // configuration and will return success result.
+    if (!wm.autoConnect()) {
         Serial.println("Failed to connect");
-        // ESP.restart();
-    } else {
-        //if you get here you have connected to the WiFi
-        Serial.println("connected...yeey :)");
+        ESP.restart();
     }
 
     meter = new FlowMeter(digitalPinToInterrupt(FLOW_PIN), UncalibratedSensor, meterCount, RISING);
