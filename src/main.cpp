e69c8be9c3e02c2f050235b4fa9119c204e63dff
Lóránt Pintér <lorant.pinter@gmail.com>
2021-10-27 15:26:57 +0200

    Wake up on flow

--- src/main.cpp
+++ src/main.cpp
@@ -2,13 +2,20 @@
 #include <Arduino.h>
 #include <FlowMeter.h>
 #include <Wire.h>
+#include <chrono>
+
+using namespace std::chrono;
 
 const gpio_num_t FLOW_PIN = GPIO_NUM_33;
+
+const auto MEASUREMENT_PERIOD = seconds { 1 };
+const auto NO_FLOW_TIMEOUT = seconds { 5 };
+const auto SLEEP_PERIOD = minutes { 1 };
 
 // get a new FlowMeter instance for an uncalibrated flow sensor
 FlowMeter* meter;
 
-long long lastSeenFlow = 0;
+auto lastSeenFlow = milliseconds::zero();
 
 IRAM_ATTR void meterCount() {
     meter->count();
@@ -24,22 +31,22 @@ void setup() {
 
 void loop() {
     // process the (possibly) counted ticks
-    delay(1000);
-    meter->tick(1000);
+    auto measurementMillis = duration_cast<milliseconds>(MEASUREMENT_PERIOD).count();
+    delay(measurementMillis);
+    meter->tick(measurementMillis);
 
     double flowRate = meter->getCurrentFlowrate();
-    auto currentTime = millis();
-    if (flowRate > 0.0) {
-        Serial.print("Flow rate: ");
-        Serial.println(flowRate);
-        lastSeenFlow = currentTime;
-    } else {
-        if (currentTime - lastSeenFlow > 5000) {
+    auto currentTime = milliseconds { millis() };
+    if (flowRate == 0.0) {
+        if ((currentTime - lastSeenFlow) > NO_FLOW_TIMEOUT) {
             Serial.println("No flow for 5 seconds, going to sleep");
-            // Go to deep sleep until woken up by GPIO interrupt
+            // Go to deep sleep until timeout or woken up by GPIO interrupt
+            esp_sleep_enable_timer_wakeup(duration_cast<microseconds>(SLEEP_PERIOD).count());
             esp_sleep_enable_ext0_wakeup(FLOW_PIN, digitalRead(FLOW_PIN) == LOW);
             esp_deep_sleep_start();
         }
+    } else {
+        lastSeenFlow = currentTime;
     }
 
     // output some measurement result
