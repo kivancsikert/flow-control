1aadd874c78b8534cf8f18b283f618cf49bd7917
Lóránt Pintér <lorant.pinter@gmail.com>
2022-03-20 19:23:19 +0100

    Let mode switch determine valve init state

--- src/ValveHandler.hpp
+++ src/ValveHandler.hpp
@@ -46,7 +46,6 @@ public:
         // RTC memory is reset to 0 upon power-up
         if (valveHandlerStoredState == 0) {
             Serial.println("Initializing for the first time");
-            setState(State::CLOSED);
         } else {
             Serial.println("Initializing after waking from sleep with state = " + String(valveHandlerStoredState));
             state = valveHandlerStoredState == 1
