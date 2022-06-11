78851aed80bdfbd1bb7a8a7281a64307ff34b4ff
Lóránt Pintér <lorant.pinter@gmail.com>
2022-06-11 23:30:11 +0200

    Fix MK3 build

--- src/mk3/RelayValveController.hpp
+++ src/mk3/RelayValveController.hpp
@@ -8,31 +8,41 @@ using namespace farmhub::client;
 class RelayValveController
     : public ValveController {
 public:
+    RelayValveController(milliseconds switchDuration)
+        : switchDuration(switchDuration) {
+    }
+
     void begin(gpio_num_t openPin, gpio_num_t closePin) {
         Serial.printf("Initializing relay valve controller on pins open = %d, close = %d\n", openPin, closePin);
         this->openPin = openPin;
         this->closePin = closePin;
         pinMode(openPin, OUTPUT);
         pinMode(closePin, OUTPUT);
+        reset();
     }
 
 protected:
-    void forward() override {
+    void open() override {
         digitalWrite(openPin, LOW);
         digitalWrite(closePin, HIGH);
+        delay(switchDuration.count());
+        reset();
     }
 
-    void reverse() override {
+    void close() override {
         digitalWrite(openPin, HIGH);
         digitalWrite(closePin, LOW);
+        delay(switchDuration.count());
+        reset();
     }
 
-    void stop() override {
+    void reset() override {
         digitalWrite(openPin, HIGH);
         digitalWrite(closePin, HIGH);
     }
 
 private:
+    const milliseconds switchDuration;
     gpio_num_t openPin;
     gpio_num_t closePin;
 };
